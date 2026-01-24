c1c62f427de37ef0e2c5b9247c4a6328153de32e
Borngreat Ikwutah <iagocech2056@gmail.com>
2026-01-24 16:25:16 +0100

    update commit

---
+++ src/db/connection.ts
@@ -0,0 +1,18 @@
+import { drizzle } from "drizzle-orm/neon-http";
+import { neon } from "@neondatabase/serverless";
+import postgres from "postgres";
+import { env } from "~/env";
+
+const connectionString = env.DATABASE_URL;
+
+// Create the Neon Structure
+const client = neon(connectionString);
+
+// Create the drizzle instance
+export const db = drizzle({
+  client: client,
+});
+
+export type DB = typeof db;
+
+export { client };
